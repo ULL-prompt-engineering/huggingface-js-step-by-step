dec9448bef5f07a3d53e9a3d18e79ec50c2dd430
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-08-23 14:01:49 +0100

    huggingface-learning

---
+++ huggingface.mjs
@@ -0,0 +1,20 @@
+import {HfInference } from '@huggingface/inference';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const HF_ACCESS_TOKEN = process.env.HF_ACCESS_TOKEN;
+
+const inference = new HfInference(HF_ACCESS_TOKEN);
+
+const model = "nlpconnect/vit-gpt2-image-captioning";
+const imgUrl = "https://cdn.mos.cms.futurecdn.net/HjFE8NKWuCmgfHCcndJ3rK.jpg";
+
+const response = await fetch(imgUrl);
+const imageBlob = await response.blob();
+// Use the imageToText function to get the image caption
+const result = await inference.imageToText({
+    data: imageBlob,
+    model,
+});
+
+console.log('result', result);
